7d68e8b333509a4ff4df674d4683f1917534b3f9
james_zhang <james_zhang@on-bright.com>
2026-05-06 11:53:53 +0800

    [005]

--- application/config/item/item_info/item_1.h
+++ application/config/item/item_info/item_1.h
@@ -15,7 +15,7 @@
 /***********Macro***********/
 //firmware version
 #define MINOR_VERSION    PROJECT_NUMBER
-#define PATCH_VERSION    4
+#define PATCH_VERSION    5
 
 
 /***********Enum***********/
